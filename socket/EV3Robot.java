641619343920cde0d16b98d967dfbb8d44ab119f
rex662624 <zfk.662624@gmail.com>
2018-04-23 07:53:46 +0800

    Update beep
    When user type 5, it will beep two times

--- socket/EV3Robot.java
+++ socket/EV3Robot.java
@@ -1,21 +1,33 @@
 import java.io.*;
 import java.net.*;
 
-public class EV3Robot  {
-   public static void main(String argv[]) throws Exception {
-      String clientSentence;
-      String capitalizedSentence;
-      ServerSocket welcomeSocket = new ServerSocket(5555);
+import lejos.hardware.Sound;
+import lejos.utility.Delay;
 
-      while(true) {
-         Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
-      }
-   }
+public class EV3Robot {
+	public static void main(String argv[]) throws Exception {
+		String clientSentence;
+		String capitalizedSentence;
+		ServerSocket welcomeSocket = new ServerSocket(5555);
+		
+		
+		Socket connectionSocket = welcomeSocket.accept();
+		while (true) {
+			
+			BufferedReader inFromClient = new BufferedReader(
+					new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(
+					connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			System.out.println("Received: " + clientSentence);
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			outToClient.writeBytes(capitalizedSentence);
+			
+			if(clientSentence.equals("5")){
+				 Sound.twoBeeps();
+				 Delay.msDelay(1000);
+				
+			}
+		}
+	}
 }
-
