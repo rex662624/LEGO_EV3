9fd6bd7b57f72271c9e53c3c8f9a904a2fcc879e
rex662624 <zfk.662624@gmail.com>
2018-04-23 07:44:05 +0800

    Update socket
    Using basic socket to send data between ev3 and PC

---
+++ socket/EV3Robot.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class EV3Robot  {
+   public static void main(String argv[]) throws Exception {
+      String clientSentence;
+      String capitalizedSentence;
+      ServerSocket welcomeSocket = new ServerSocket(5555);
+
+      while(true) {
+         Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+      }
+   }
+}
+
